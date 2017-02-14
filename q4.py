66015fbd2d47f45f62ef430b88b3454f96cb7f2c
Marija Robinson <marija@springmail.com>
2017-02-13 18:07:24 -0600

    Chore: Document test cases

--- q4.py
+++ q4.py
@@ -1,17 +1,19 @@
-STUDY BIG O SOME MORE
-
 # """Question 4
 # -Find the least common ancestor between two nodes on a binary search tree.
-# -The least common ancestor is the farthest node from the root that is an ancestor of both nodes.
-# -For example, the root is a common ancestor of all nodes on the tree, but if both nodes are descendents
-#  of the root's left child, then that left child might be the lowest common ancestor.
-# -You can assume that both nodes are in the tree, and the tree itself adheres to all BST properties.
+# -The least common ancestor is the farthest node from the root that is
+#   an ancestor of both nodes.
+# -For example, the root is a common ancestor of all nodes on the tree, but
+#   if both nodes are descendents of the root's left child, then that left
+#   child might be the lowest common ancestor.
+# -You can assume that both nodes are in the tree, and the tree itself
+#   adheres to all BST properties.
 
-# -The function definition should look like question4(T, r, n1, n2), where T is the tree represented as
-# a matrix, where the index of the list
-# is equal to the integer stored in that node and a 1 represents a child node, r is a non-negative integer
-# representing the root, and n1 and n2 are non-negative integers representing the two nodes in no
-# particular order. For example, one test case might be"""
+# -The function definition should look like question4(T, r, n1, n2), where T
+#   is the tree represented as a matrix, where the index of the list
+#   is equal to the integer stored in that node and a 1 represents a child node,
+#   r is a non-negative integer representing the root, and n1 and n2 are
+#   non-negative integers representing the two nodes in no
+#   particular order. For example, one test case might be"""
 
 # question4([[0, 1, 0, 0, 0],
 #            [0, 0, 0, 0, 0],
@@ -24,54 +26,87 @@ STUDY BIG O SOME MORE
 
 # """and the answer would be 3.""" (least common ancestor)
 
-# So, the answer should be common ancestor to 1, 4. Zero is not a common ancestor because it's only
-# an ancestor to 0
-
-# This is the complete working code
-# Work in progress, It works, but always trying to improve
 
 # Assumptions:
 # Do not need to make a BST first
+# Will return None if no common ancestor found
+# Both nodes provided are in the tree
+
+# Test Cases:
+#   Udacity's case
+#   print question4([[0, 1, 0, 0, 0],
+#                    [0, 0, 0, 0, 0], [0, 0, 0, 0, 0],
+#                    [1, 0, 0, 0, 1], [0, 0, 0, 0, 0]],
+#                     3,
+#                     1,
+#                     4)
+#   Should return the root, 3
+
+#   print question4([[0, 1, 1, 0, 0, 0],
+#                    [0, 0, 0, 0, 0, 0],
+#                    [0, 0, 0, 0, 1, 1],
+#                    [1, 0, 0, 0, 0, 0],
+#                    [0, 0, 0, 0, 0, 0],
+#                    [0, 0, 0, 0, 0, 0]],
+#                     3,
+#                     4,
+#                     5)
+#   I included this case because it has two common ancestors, and I wanted to
+#   make sure my algorithm returns the *least* common ancestor
+#   This should return 2
+
+#   print question4([[0, 1, 1, 0, 0, 0],
+#                    [0, 0, 0, 0, 0, 0],
+#                    [0, 0, 0, 0, 1, 1],
+#                    [1, 0, 0, 0, 0, 0],
+#                    [0, 0, 0, 0, 0, 0],
+#                    [0, 0, 0, 0, 0, 0]],
+#                     3,
+#                     1,
+#                     5)
+#   Testing with the same tree, but one of the ancestors being up a level
+#   Should return 0
 
 def question4(T, r, n1, n2):
-    ptRel, n1Ancestors, n2Ancestors = [], [], []
-    n1Next, n2Next = None, None
+    # Create a list of dictionaries to hold all relationships
+    # and a list for each of the n1 and n2 ancestors for comparison
+    pc_rel, n1_ancestors, n2_ancestors = [], [], []
+    # Create variables to hold the next item in the tree for comparison
+    n1_next, n2_next = None, None
+    # Go through each item in the maxtrix and determine parent-child
+    # relationships and append each pair to pC_Rel list
     for t in T:
+        # Go through the inner list and check if the node contains a child node
+        # if so, append to the parent/child list
         for m in range(len(t)):
             if t[m] == 1:
-                ptRel.append({'parent': T.index(t), 'child': m})
-    for p in ptRel:
+                pc_rel.append({'parent': T.index(t), 'child': m})
+    # Look at the parent/child list and create a list of parents
+    # for each child that was given as n1 or n2
+    for p in pc_rel:
         if p['child'] == n1:
-            n1Ancestors.append(p['parent'])
-            n1Next = p['parent']
-            for p in ptRel:
-                if p['child'] == n1Next:
-                    n1Ancestors.append(p['parent'])
+            n1_ancestors.append(p['parent'])
+            n1_next = p['parent']
+            for p in pc_rel:
+                if p['child'] == n1_next:
+                    n1_ancestors.append(p['parent'])
         if p['child'] == n2:
-            n2Ancestors.append(p['parent'])
-            n2Next = p['parent']
-            for p in ptRel:
-                if p['child'] == n2Next:
-                    n2Ancestors.append(p['parent'])
-    x, y = 0, 0
-    try:
-        while n1Ancestors[x] != n2Ancestors[y]:
-            y += 1
-            try:
-                while n1Ancestors[x] != n2Ancestors[y]:
-                        x += 1
-            except IndexError:
-                print "out of items"
-            else:
-                return n1Ancestors[x]
-    except IndexError:
-        print "out of items"
-    else:
-        return n1Ancestors[x]
+            n2_ancestors.append(p['parent'])
+            n2_next = p['parent']
+            for p in pc_rel:
+                if p['child'] == n2_next:
+                    n2_ancestors.append(p['parent'])
+    # Now we will compare the lists and find the common ancestors, then
+    # return the lowest in the tree (that closest to the beginning of the list)
+    any_in = [x for x in n1_ancestors if x in n2_ancestors]
+    if any_in:
+        return any_in[0]
 
-
+print question4([[0, 1, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0],
+                 [1, 0, 0, 0, 1], [0, 0, 0, 0, 0]], 3, 1, 4)
 print question4([[0, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 1],
                  [1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0],
                  [0, 0, 0, 0, 0, 0]], 3, 4, 5)
-print question4([[0, 1, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0],
-                 [1, 0, 0, 0, 1], [0, 0, 0, 0, 0]], 3, 1, 4)
+print question4([[0, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 1],
+                 [1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0]], 3, 1, 5)
